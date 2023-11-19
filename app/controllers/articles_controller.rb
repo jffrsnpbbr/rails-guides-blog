9f01277d38eb6d62b6a1744f791a559a93e1bd72
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-11-19 09:59:26 +0800

    Updating an Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,18 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    end
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
